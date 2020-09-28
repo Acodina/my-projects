306e21982a010f2f9a6bac28f41982913838ad1a
Anna Codina <anna.co91@gmail.com>
2020-09-28 18:11:28 +0200

    add test in chart-react-flux action

--- chart-react-flux/src/actions/ProductActions.test.js
+++ chart-react-flux/src/actions/ProductActions.test.js
@@ -1,19 +1,18 @@
-import dispatcher from '../dispatcher';
-import actionTypes from './actionTypes';
-import { getProductList } from './ProductActions';
+import dispatcher from "../dispatcher";
+import { getProductList } from "./ProductActions";
+import mockList from "../mocks/products";
 
-jest.mock('../dispatcher');
-jest.dontMock('./ProductActions');
+jest.mock("../dispatcher");
+jest.dontMock("./ProductActions");
 
-describe('ProductActions', () => {
-	xit('Should return product List', async () => {
-		const fakeList = ['fakeList'];
-		jest.spyOn(global, 'fetch').mockImplementation(() => {
-			return Promise.resolve({
-				json: () => Promise.resolve(fakeList)
-			});
-		});
-		await getProductList();
-		expect(dispatcher.dispatch.mock.calls[0][0].data).toEqual(fakeList);
-	});
+describe("ProductActions", () => {
+  const productId = 1;
+  it("Should return product List", async () => {
+    jest.spyOn(global, "fetch").mockImplementation(() => {
+      return Promise.resolve(mockList);
+    });
+    await getProductList();
+    const firstResult = dispatcher.dispatch.mock.calls[0][0].data[0];
+    expect(firstResult.id).toEqual(productId);
+  });
 });
